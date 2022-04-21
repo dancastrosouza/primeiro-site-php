78207ee9efb2b996548dd0eb5a7a7bdf1a45bb22
Danilo Alencastro <104075718+dancastrosouza@users.noreply.github.com>
2022-04-21 17:01:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+ <html>
+ 
+ 
+ 
+      <head>
+	        <title>Meu primeiro site!!</title>
+	 </head>
+	 
+	   <body>
+	        
+			<?php
+			   for ( $i = 0 ; $i < 10 ; $i++ ) {
+				 printf( "Linha número " . $i . "<br />");
+			   }
+     ?>
+	 
+ </body>
+ 
+ </html>
+			   
+
+	
